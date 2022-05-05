85f018ccc5dbd614da51971adaa5c880d520ea1c
Feichtmeier <frederik.feichtmeier@gmail.com>
2022-05-05 15:48:19 +0200

    Experiments with the AppBar without GtkHeaderbar

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -1,10 +1,23 @@
-import 'dart:math' as math;
-
 import 'package:flutter/material.dart';
+import 'package:screen_retriever/screen_retriever.dart';
 import 'package:url_launcher/url_launcher_string.dart';
+import 'package:window_manager/window_manager.dart';
 import 'package:yaru/yaru.dart';
+import 'package:yaru_icons/yaru_icons.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  // Must add this line.
+  await windowManager.ensureInitialized();
+
+  WindowOptions windowOptions = WindowOptions(
+    size: const Size(800, 600),
+    skipTaskbar: false,
+  );
+  windowManager.waitUntilReadyToShow(windowOptions, () async {
+    await windowManager.show();
+    await windowManager.focus();
+  });
   runApp(
     MaterialApp(
       theme: yaruLight,
@@ -21,28 +34,165 @@ class HandyPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('Handy Window'),
-      ),
-      body: const Center(
-        child: ClickableLink(
-          url: 'https://gnome.pages.gitlab.gnome.org/libhandy',
-          child: Text(
-            'gitlab.gnome.org/libhandy',
-            style: TextStyle(fontSize: 28),
-          ),
+      appBar: YaruAppBar(
+          title: const Text('Flutter AppBar'),
+          appBarHeight: Theme.of(context).appBarTheme.toolbarHeight ?? 48),
+      body: Center(
+        child: Column(
+          mainAxisSize: MainAxisSize.min,
+          children: const [
+            ClickableLink(
+              url:
+                  'https://github.com/ubuntu-flutter-community/handy_window/tree/import',
+              child: Text(
+                'handy_window',
+                style: TextStyle(fontSize: 28),
+              ),
+            ),
+            ClickableLink(
+              url: 'https://github.com/ubuntu/yaru_widgets.dart',
+              child: Text(
+                'yaru_widgets.dart',
+                style: TextStyle(fontSize: 28),
+              ),
+            ),
+            ClickableLink(
+              url: 'https://github.com/ubuntu/yaru.dart',
+              child: Text(
+                'yaru.dart',
+                style: TextStyle(fontSize: 28),
+              ),
+            )
+          ],
         ),
-      ),
-      bottomNavigationBar: Row(
-        children: const [
-          RotatedArrow(angle: math.pi / 4),
-          Spacer(),
-          RotatedArrow(angle: -math.pi / 4),
-        ],
       ),
     );
   }
 }
+
+final screenRetriever = ScreenRetriever.instance;
+
+class YaruAppBar extends StatefulWidget implements PreferredSizeWidget {
+  final double appBarHeight;
+  final Widget title;
+
+  const YaruAppBar({Key? key, required this.appBarHeight, required this.title})
+      : super(key: key);
+
+  @override
+  State<YaruAppBar> createState() => _YaruAppBarState();
+
+  @override
+  Size get preferredSize => Size(0, appBarHeight);
+}
+
+class _YaruAppBarState extends State<YaruAppBar>
+    with WindowListener, ScreenListener {
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      // onLongPress: () async => await WindowManager.instance
+      //     .setPosition(await screenRetriever.getCursorScreenPoint()),
+      onLongPressEnd: (_) async => await WindowManager.instance
+          .setPosition(await screenRetriever.getCursorScreenPoint()),
+      onDoubleTap: () async => await WindowManager.instance.isMaximized()
+          ? await WindowManager.instance.unmaximize()
+          : await WindowManager.instance.maximize(),
+      child: AppBar(
+        title: widget.title,
+        backgroundColor: const Color.fromARGB(255, 236, 236, 236),
+        actions: const [
+          TitleButton(
+            iconData: YaruIcons.window_minimize,
+            titleButtonAction: TitleButtonAction.minimize,
+          ),
+          TitleButton(
+              iconData: YaruIcons.window_maximize,
+              titleButtonAction: TitleButtonAction.maximize),
+          TitleButton(
+            iconData: YaruIcons.window_close,
+            titleButtonAction: TitleButtonAction.close,
+          ),
+        ],
+      ),
+    );
+  }
+
+  @override
+  void initState() {
+    screenRetriever.addListener(this);
+    windowManager.addListener(this);
+    super.initState();
+  }
+
+  @override
+  void dispose() {
+    windowManager.removeListener(this);
+    super.dispose();
+  }
+}
+
+class TitleButton extends StatefulWidget {
+  const TitleButton({
+    Key? key,
+    required this.iconData,
+    required this.titleButtonAction,
+  }) : super(key: key);
+
+  final IconData iconData;
+  final TitleButtonAction titleButtonAction;
+
+  @override
+  State<TitleButton> createState() => _TitleButtonState();
+}
+
+class _TitleButtonState extends State<TitleButton> {
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.all(9.0),
+      child: SizedBox(
+        height: 30,
+        width: 30,
+        child: Material(
+          color: const Color.fromARGB(255, 223, 223, 223),
+          borderRadius: BorderRadius.circular(100),
+          child: InkWell(
+            borderRadius: BorderRadius.circular(100),
+            onTap: () async {
+              switch (widget.titleButtonAction) {
+                case TitleButtonAction.close:
+                  await close();
+                  break;
+                case TitleButtonAction.maximize:
+                  await maximize();
+                  break;
+                case TitleButtonAction.minimize:
+                  await minimize();
+                  break;
+                default:
+              }
+            },
+            child: Icon(
+              widget.iconData,
+              size: 20,
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+enum TitleButtonAction { close, minimize, maximize }
+
+Future<void> close() async => await WindowManager.instance.close();
+
+Future<void> maximize() async => await WindowManager.instance.isMaximized()
+    ? await WindowManager.instance.unmaximize()
+    : await WindowManager.instance.maximize();
+
+Future<void> minimize() async => await WindowManager.instance.minimize();
 
 class ClickableLink extends StatelessWidget {
   const ClickableLink({
--- example/linux/my_application.cc
+++ example/linux/my_application.cc
@@ -38,11 +38,11 @@ static void my_application_activate(GApplication* application) {
   }
 #endif
   if (use_header_bar) {
-    GtkHeaderBar* header_bar = GTK_HEADER_BAR(gtk_header_bar_new());
-    gtk_widget_show(GTK_WIDGET(header_bar));
-    gtk_header_bar_set_title(header_bar, "Handy Window");
-    gtk_header_bar_set_show_close_button(header_bar, TRUE);
-    gtk_window_set_titlebar(window, GTK_WIDGET(header_bar));
+    // GtkHeaderBar* header_bar = GTK_HEADER_BAR(gtk_header_bar_new());
+    // gtk_widget_show(GTK_WIDGET(header_bar));
+    // gtk_header_bar_set_title(header_bar, "Handy Window");
+    // gtk_header_bar_set_show_close_button(header_bar, TRUE);
+    // gtk_window_set_titlebar(window, GTK_WIDGET(header_bar));
   } else {
     gtk_window_set_title(window, "Handy Window");
   }
